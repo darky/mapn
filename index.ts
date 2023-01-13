841e82b34cd45996c3abdd3b02024f1b724204df
Vladislav Botvin <darkvlados@gmail.com>
2023-01-14 00:01:07 +0300

    skip

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
-export function mapn<T, R0>(arr: T[], fn: (el: T) => [R0]): [R0[]];
-export function mapn<T, R0, R1>(arr: T[], fn: (el: T) => [R0, R1]): [R0[], R1[]];
-export function mapn<T, R0, R1, R2>(arr: T[], fn: (el: T) => [R0, R1, R2]): [R0[], R1[], R2[]];
-export function mapn<T, R0, R1, R2, R3>(arr: T[], fn: (el: T) => [R0, R1, R2, R3]): [R0[], R1[], R2[], R3[]];
+type Skip = symbol & { __brand: 'mapnSkip' };
+export const mapnSkip = Symbol('mapnSkip') as Skip;
+
+export function mapn<T, R0>(arr: T[], fn: (el: T) => [R0]): [Exclude<R0, Skip>[]];
+
+export function mapn<T, R0, R1>(arr: T[], fn: (el: T) => [R0, R1]): [Exclude<R0, Skip>[], Exclude<R1, Skip>[]];
+
+export function mapn<T, R0, R1, R2>(
+  arr: T[],
+  fn: (el: T) => [R0, R1, R2]
+): [Exclude<R0, Skip>[], Exclude<R1, Skip>[], Exclude<R2, Skip>[]];
+
+export function mapn<T, R0, R1, R2, R3>(
+  arr: T[],
+  fn: (el: T) => [R0, R1, R2, R3]
+): [Exclude<R0, Skip>[], Exclude<R1, Skip>[], Exclude<R2, Skip>[], Exclude<R3, Skip>[]];
+
 export function mapn<T, R0, R1, R2, R3, R4>(
   arr: T[],
   fn: (el: T) => [R0, R1, R2, R3, R4]
-): [R0[], R1[], R2[], R3[], R4[]];
+): [Exclude<R0, Skip>[], Exclude<R1, Skip>[], Exclude<R2, Skip>[], Exclude<R3, Skip>[], Exclude<R4, Skip>[]];
+
 export function mapn(arr: unknown[], fn: (el: unknown) => unknown[]) {
   const response: Record<number, unknown[]> = {
     0: [],
@@ -16,7 +30,7 @@ export function mapn(arr: unknown[], fn: (el: unknown) => unknown[]) {
   };
 
   for (const el of arr) {
-    fn(el).forEach((resp, i) => response[i]!.push(resp));
+    fn(el).forEach((resp, i) => resp === mapnSkip || response[i]!.push(resp));
   }
 
   return Object.values(response);
--- test.ts
+++ test.ts
@@ -1,9 +1,15 @@
 import assert from 'node:assert';
 import { test } from 'node:test';
-import { mapn } from '.';
+import { mapn, mapnSkip } from '.';
 
 test('simple', () => {
   const [numbers, strings] = mapn([1, 2, 3], n => [n * 2, `${n}`]);
   assert.deepStrictEqual(numbers, [2, 4, 6]);
+  assert.deepStrictEqual(strings, ['1', '2', '3']);
+});
+
+test('skip', () => {
+  const [numbers, strings] = mapn([1, 2, 3], n => [n === 2 ? mapnSkip : n * 2, `${n}`]);
+  assert.deepStrictEqual(numbers, [2, 6]);
   assert.deepStrictEqual(strings, ['1', '2', '3']);
 });
