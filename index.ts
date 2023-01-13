9b278eb2aea7a91485b2d34bea1187d4306ff22d
Vladislav Botvin <darkvlados@gmail.com>
2023-01-14 00:15:47 +0300

    flatten code for speed

--- index.ts
+++ index.ts
@@ -21,17 +21,25 @@ export function mapn<T, R0, R1, R2, R3, R4>(
 ): [Exclude<R0, Skip>[], Exclude<R1, Skip>[], Exclude<R2, Skip>[], Exclude<R3, Skip>[], Exclude<R4, Skip>[]];
 
 export function mapn(arr: unknown[], fn: (el: unknown) => unknown[]) {
-  const response: Record<number, unknown[]> = {
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-  };
+  const resp0: unknown[] = [];
+  const resp1: unknown[] = [];
+  const resp2: unknown[] = [];
+  const resp3: unknown[] = [];
+  const resp4: unknown[] = [];
 
   for (const el of arr) {
-    fn(el).forEach((resp, i) => resp === mapnSkip || response[i]!.push(resp));
+    const fnResp = fn(el);
+    const fnResp0 = fnResp[0];
+    fnResp0 === void 0 || fnResp0 === mapnSkip || resp0.push(fnResp0);
+    const fnResp1 = fnResp[1];
+    fnResp1 === void 0 || fnResp1 === mapnSkip || resp1.push(fnResp1);
+    const fnResp2 = fnResp[2];
+    fnResp2 === void 0 || fnResp2 === mapnSkip || resp2.push(fnResp2);
+    const fnResp3 = fnResp[3];
+    fnResp3 === void 0 || fnResp3 === mapnSkip || resp3.push(fnResp3);
+    const fnResp4 = fnResp[4];
+    fnResp4 === void 0 || fnResp4 === mapnSkip || resp4.push(fnResp4);
   }
 
-  return Object.values(response);
+  return [resp0, resp1, resp2, resp3, resp4];
 }
